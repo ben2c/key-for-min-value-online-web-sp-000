7013ca67e878c1b4c3f54637eeec7d3f29899bc1
Benjamin Cheng <bencheng200@gmail.com>
2020-11-12 23:23:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
       lowest = key
       count +=1
     else
-      if name_hash[lowest] < name_hash[key]
+      if name_hash[lowest] > name_hash[key]
         lowest = key
       end
     end
